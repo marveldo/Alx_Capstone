67adca0bf8c16e1a32ff294e73eaf5f0c424441c
utibe solomon <utibesolomon6@gmail.com>
2025-01-12 23:15:56 +0100

    Final Changes

--- capstoneproject/settings.py
+++ capstoneproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alx-capstone-fw3gi1syy-marveldos-projects.vercel.app']
 
 
 # Application definition
